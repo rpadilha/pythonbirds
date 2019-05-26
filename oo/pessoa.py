05bfec21d184fe82a8d8bea030db27e7e21ae6c9
Renato Padilha <tonare@gmail.com>
2019-05-26 16:50:40 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,8 +1,12 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=None):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
-        return f'Olá, {id(self)}'
+        return f'Olá, {self.nome}! Você tem {self.idade} anos.'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Renato', 39)
     print(p.cumprimentar())
