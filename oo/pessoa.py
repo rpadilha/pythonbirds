58dbaf022dc9e7b9a0ef6c18dcbfd08ae46f346a
Renato Padilha <tonare@gmail.com>
2019-05-26 17:45:24 -0300

    Criados método de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome=None, idade=None):
         self.idade = idade
         self.nome = nome
@@ -7,6 +9,14 @@ class Pessoa:
     def cumprimentar(self):
         return f'Olá, {self.nome}! Você tem {self.idade} anos.'
 
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributos_de_classe(cls):
+        return f'{cls} - OLHOS {cls.olhos}'
+
 
 if __name__ == '__main__':
     Renato = Pessoa(nome='Renato', idade=39)
@@ -14,5 +24,15 @@ if __name__ == '__main__':
     print(Luiz.cumprimentar())
     print('*'*20)
 
+    print(id(Renato.olhos), id(Luiz.olhos), id(Pessoa.olhos))
+    print('*'*20)
+
+    print(Renato.olhos)
+    print('*'*20)
+
     for filho in Luiz.filhos:
         print(filho.nome)
+
+    print('*'*20)
+    print(Pessoa.metodo_estatico(), Luiz.metodo_estatico())
+    print(Pessoa.nome_e_atributos_de_classe(), Luiz.nome_e_atributos_de_classe())
