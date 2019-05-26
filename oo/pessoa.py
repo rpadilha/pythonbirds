5310f8e0df6ff756c6c9374d3bc4c5649eea4a22
Renato Padilha <tonare@gmail.com>
2019-05-26 16:59:23 -0300

    Criados atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,12 +1,18 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=None):
+    def __init__(self, *filhos, nome=None, idade=None):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá, {self.nome}! Você tem {self.idade} anos.'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Renato', 39)
-    print(p.cumprimentar())
+    Renato = Pessoa(nome='Renato', idade=39)
+    Luiz = Pessoa(Renato, nome='Luiz', idade=39)
+    print(Luiz.cumprimentar())
+    print('*'*20)
+
+    for filho in Luiz.filhos:
+        print(filho.nome)
